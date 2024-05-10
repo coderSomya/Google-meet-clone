035220abb236ba6fd4432e490d0bcf16aab6c4b3
Somyajeet <isomya13@gmail.com>
2024-05-10 17:10:21 +0530

    chore: basic client and server setup.

---
+++ server/src/index.ts
@@ -0,0 +1,34 @@
+import express from "express";
+import {Server} from "socket.io"
+import http from "http";
+import cors from "cors";
+
+import ServerConfig from "./config/serverConfig";
+
+const app = express();
+
+app.use(cors())
+
+
+
+//node server
+const server = http.createServer(app);
+server.listen(ServerConfig.PORT, ()=>{
+    console.log(`Server is listening on ${ServerConfig.PORT}`);
+});
+
+
+//socket server
+const io = new Server(server, {
+    cors:{
+        origin: "*",
+        methods: ["GET", "POST"]
+    }
+})
+io.on("connection", (socket)=>{
+    console.log("new user connected");
+
+    socket.on("disconnect", ()=>{
+        console.log("user disconnected");
+    })
+})
